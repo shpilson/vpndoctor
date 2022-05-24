98ce51fa79470dad1cb87d36aad608a7b1ce8787
shpilson <kovalenko.jora@gmail.com>
2022-05-24 14:14:23 +0300

    Отображение IP-адреса пользователя

---
+++ js/ip.js
@@ -0,0 +1,24 @@
+fetch('https://ipapi.co/json/')
+    .then(function (response) {
+        response.json().then(jsonData => {
+            var statusDiv = document.getElementById('user-status');
+            var locationDiv = document.getElementById('user-location');
+            var ipDiv = document.getElementById('user-ip');
+            var hostnameDiv = document.getElementById('user-hostname');
+
+            if (jsonData.country_name != 'Russia') {
+                statusDiv.innerHTML = 'Защищено';
+            } else {
+                statusDiv.innerHTML = 'Не защищено';
+            }
+
+            locationDiv.innerHTML = jsonData.country_name + ', ' + jsonData.city;
+            ipDiv.innerHTML = jsonData.ip;
+            hostnameDiv.innerHTML = jsonData.org;
+
+            console.log(jsonData);
+        });
+    })
+    .catch(function (error) {
+        console.log(error)
+    });
